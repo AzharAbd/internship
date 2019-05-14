a766dc6572691393d7012b00da72d0f98562a2b9
AzharAbd <azhar.razar@gmail.com>
2019-05-14 11:23:41 +0700

    Initial commit

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+exports.index = function(req, res, next) {
+  res.render('index', { title: 'Express' });
+}
+
+exports.azhar = function(req, res, next) {
+    res.render('index', { title: 'Azhar' });
+  }
