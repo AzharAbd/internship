8891a8dd5c669558506044294ee7546d608d8109
AzharAbd <azhar.razar@gmail.com>
2019-05-14 12:57:44 +0700

    Form

--- controllers/page.js
+++ controllers/page.js
@@ -1,11 +1,17 @@
 exports.landing = function(req, res, next) {
-  res.render('landing', { title: 'Express' });
+  res.render('landing', { title: 'Express', nama: '', email :'' });
 }
 
 exports.azhar = function(req, res, next) {
-    res.render('landing', { title: 'Azhar' });
+  res.render('landing', { title: 'Azhar', nama: '', email :'' });
 }
 
-exports.abdurrasyid = function(req, res, next) {
-  res.render('landing', { title: 'Abdurrasyid' });
+exports.submit_lead = function(req, res, next) {
+  console.log("email:", req.body.email);
+  console.log("nama:",req.body.nama);
+  console.log("masuk");
+  res.render('landing', { title: 'Submit' , nama: req.body.nama, email :req.body.email});
+}
+exports.v2 = function(req, res, next) {
+  res.render('submit');
 }
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ var router = express.Router();
 let page = require('../controllers/page');
 /* GET home page. */
 router.get('/', page.landing);
-
 router.get('/azhar', page.azhar);
+router.get('/submit', page.v2);
 
-router.get('/abdurrasyid', page.abdurrasyid);
+// router.post('/', page.submit_lead);
 
 module.exports = router;
