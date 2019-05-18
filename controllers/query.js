cc34499602437bac5fbae45fe1940003ff4b5621
AzharAbd <azhar.razar@gmail.com>
2019-05-18 13:46:02 +0700

    done

--- controllers/query.js
+++ controllers/query.js
@@ -1,51 +1,55 @@
-// const Pool = require('pg').Pool
-// const pool = new Pool({
-//   user: 'myapi',
-//   host: 'localhost',
-//   database: 'myapi',
-//   password: 'azhar1e3',
-//   port: 5432,
-// })
-
 var pg = require('pg');
 var pool = new pg.Client("postgres://myapi:azhar1e3@localhost:5432/myapi");
 pool.connect();
 
-const getArticles = function (req, resp, check) {    
+const getArticlesbyID = function (keyword, source, resp){
+    pool.query("SELECT * FROM article WHERE description LIKE $1 AND source_name LIKE $2 order by id;", [keyword,source],  function (error, results) {
+        if (error) {
+            throw error
+        }
+        if (results.rows.length != 0){
+            resp.status(200).json(results.rows);
+        } else {
+            resp.status(200).json();
+        }
+    })
+}
+
+const getArticlesbyPublish = function (keyword, source, resp){
+    pool.query("SELECT * FROM article WHERE description LIKE $1 AND source_name LIKE $2 order by publishedAt;", [keyword,source],  function (error, results) {
+        if (error) {
+            throw error
+        }
+        if (results.rows.length != 0){
+            resp.status(200).json(results.rows);
+        } else {
+            resp.status(200).json();
+        }
+    })
+}
+
+const initializeVar = function (req, resp, check) {    
     if (check){
         var keyword;
         var source;
         var persen = '%';
-        if (req.query.keyword === ""){
+        if (req.query.q === ""){
             keyword = persen;
-            console.log("Keyword is empty");
         } else {
-            keyword = persen.concat(req.query.keyword,persen);
-            console.log("Keyword is NOT empty");
+            keyword = persen.concat(req.query.q,persen);
         }
         console.log("keyword:", keyword);
         if (req.query.source === ""){
             source = persen;
-            console.log("Source is empty");
         } else {
             source = req.query.source;
-            console.log("Source is NOT empty");
         }
-        sort_by = req.query.sort_by;
-        console.log("order by:", sort_by);
-        console.log("source:",source);
-        pool.query("SELECT * FROM article WHERE description LIKE $1 AND source_name LIKE $2 order by publishedAt;", [keyword,source],  function (error, results) {
-            if (error) {
-                throw error
-            }
-            if (results.rows.length != 0){
-                console.log("Data tersedia");
-                resp.status(200).json(results.rows);
-            } else {
-                console.log("data tidak ada");
-                resp.status(200).json();
-            }
-        })
+        if (req.query.sortBy == "id"){
+            getArticlesbyID(keyword, source, resp);
+        } else {
+            getArticlesbyPublish(keyword, source, resp);
+        }
+        
     } else {
         console.log("api key salah");
         resp.status(200).json();
@@ -53,7 +57,7 @@ const getArticles = function (req, resp, check) {
 }
 
 const checkKey = function (req, resp){
-    var a_key = req.query.api_key;
+    var a_key = req.query.key;
     const myPromise = new Promise(function(resolve, reject){
         pool.query('select * from api_key where key = $1', [a_key], function (error, results){
             if (error){
@@ -68,7 +72,7 @@ const checkKey = function (req, resp){
     myPromise
     .then(function resolved(response) {
       console.log(response);
-      getArticles(req,resp, response);
+      initializeVar(req,resp, response);
     })
     .catch(function rejected(err) {
       console.error(err)
@@ -76,7 +80,6 @@ const checkKey = function (req, resp){
 }
 
 module.exports = {
-    getArticles,
     checkKey
 }
 
